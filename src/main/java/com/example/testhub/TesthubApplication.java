4ead6719c33848dd00098c118cc288494445ac6d
nuluf <orehappy_k7417@icloud.com>
2026-03-16 22:05:17 +0900

    initial commit

---
+++ src/main/java/com/example/testhub/TesthubApplication.java
@@ -0,0 +1,12 @@
+package com.example.testhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesthubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesthubApplication.class, args);
+    }
+}
