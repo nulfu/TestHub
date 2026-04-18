d7e3dc1a8b49bb5dfcdaa644cd7c88c2a4f98cb5
nuluf <orehappy_k7417@icloud.com>
2026-04-18 14:50:02 +0900

    add TesthubApplication

--- src/main/java/com/example/testhub/TesthubApplication.java
+++ src/main/java/com/example/testhub/TesthubApplication.java
@@ -1,7 +1,16 @@
 package com.example.testhub;
 
+import java.util.UUID;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.testhub.domain.project.ProjectId;
+import com.example.testhub.domain.release.Release;
+import com.example.testhub.domain.release.ReleaseRepository;
+import com.example.testhub.domain.release.ReleaseId;
 
 @SpringBootApplication
 public class TesthubApplication {
@@ -9,5 +18,24 @@ public class TesthubApplication {
     public static void main(String[] args) {
         SpringApplication.run(TesthubApplication.class, args);
     }
+
+    @Bean
+CommandLineRunner init(
+    ReleaseRepository releaseRepo
+) {
+    return args -> {
+
+        Release release = new Release(
+            new ReleaseId(UUID.randomUUID()),
+            new ProjectId(UUID.randomUUID()),
+            "リリース1"
+        );
+
+        releaseRepo.save(release);
+
+        System.out.println("==== ReleaseID ====");
+        System.out.println(release.getId().getValue());
+    };
+}
 
 }
